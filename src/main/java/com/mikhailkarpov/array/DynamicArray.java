117987bc5b4683fe5b2640ef3c78d35d403626ca
Mikhail Karpov <mikhailkarpov@hotmail.com>
2020-02-22 12:24:59 +0200

    Initial commit

---
+++ src/main/java/com/mikhailkarpov/array/DynamicArray.java
@@ -0,0 +1,97 @@
+package com.mikhailkarpov.array;
+
+public class DynamicArray<T> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private T[] arr;
+    private int size;
+
+    public DynamicArray() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public DynamicArray(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Illegal capacity: " + capacity);
+        }
+        arr = (T[]) new Object[capacity];
+    }
+
+    public void add(T elem) {
+        int length = arr.length;
+        if (length == size) {
+            T[] new_arr = (T[]) new Object[length * 2];
+            System.arraycopy(arr, 0, new_arr, 0, length);
+            arr = new_arr;
+        }
+        arr[size] = elem;
+        size++;
+    }
+
+    public void clear() {
+        arr = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public boolean contains(T elem) {
+        return indexOf(elem) != -1;
+    }
+
+    public T get(int index) {
+        checkIndexValid(index);
+        return arr[index];
+    }
+
+    public int indexOf(T elem) {
+        for (int i = 0; i < arr.length; i++) {
+            if (elem.equals(arr[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean remove(T elem) {
+        int index = indexOf(elem);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    public void remove(int index) {
+        checkIndexValid(index);
+
+        T[] new_arr = (T[]) new Object[arr.length - 1];
+
+        for (int i =0, j = 0; i < arr.length; i++, j++) {
+            if (i == index) {
+                j--;
+            } else {
+                new_arr[j] = arr[i];
+            }
+        }
+        arr = new_arr;
+        size--;
+    }
+
+    public void set(int index, T elem) {
+        checkIndexValid(index);
+        arr[index] = elem;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void checkIndexValid(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+    }
+}
---
+++ src/test/java/com/mikhailkarpov/array/DynamicArrayTest.java
@@ -0,0 +1,130 @@
+package com.mikhailkarpov.array;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DynamicArrayTest {
+
+    DynamicArray<String> dynamicArray;
+    String FIRST_TEST_STRING = "string";
+    String SECOND_TEST_STRING = "another string";
+    String THIRD_TEST_STRING = "one more string";
+
+    @Before
+    public void setUp() throws Exception {
+        dynamicArray = new DynamicArray<>();
+
+        dynamicArray.add(FIRST_TEST_STRING);
+        dynamicArray.add(SECOND_TEST_STRING);
+    }
+
+    @Test
+    public void defaultConstructor_shouldCreateEmptyArray() {
+        dynamicArray = new DynamicArray<>();
+
+        assertTrue(dynamicArray.isEmpty());
+        assertEquals(0, dynamicArray.size());
+    }
+
+    @Test
+    public void constructorWithCapacity_shouldCreateEmptyArray() {
+        dynamicArray = new DynamicArray<>(32);
+
+        assertTrue(dynamicArray.isEmpty());
+        assertEquals(0, dynamicArray.size());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructorWithIllegalCapacity_shouldThrowIllegalArgumentException() {
+        dynamicArray = new DynamicArray<>(-1);
+    }
+
+    @Test
+    public void addElements_shouldAddElement() {
+        dynamicArray.add(THIRD_TEST_STRING);
+
+        assertTrue(dynamicArray.contains(THIRD_TEST_STRING));
+        assertEquals(THIRD_TEST_STRING, dynamicArray.get(2));
+    }
+
+    @Test
+    public void addElement_shouldIncreaseSize() {
+        dynamicArray.add(THIRD_TEST_STRING);
+
+        assertFalse(dynamicArray.isEmpty());
+        assertEquals(3, dynamicArray.size());
+    }
+
+    @Test
+    public void getByIndex_shouldReturnElement() {
+        assertEquals(FIRST_TEST_STRING, dynamicArray.get(0));
+        assertEquals(SECOND_TEST_STRING, dynamicArray.get(1));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void getByIllegalIndex_shouldThrowIndexOutOfBoundsException() {
+        dynamicArray.get(2);
+    }
+
+    @Test
+    public void set_byIndex_shouldSetNewElement() {
+        dynamicArray.set(0, SECOND_TEST_STRING);
+        dynamicArray.set(1, THIRD_TEST_STRING);
+
+        assertEquals(SECOND_TEST_STRING, dynamicArray.get(0));
+        assertEquals(THIRD_TEST_STRING, dynamicArray.get(1));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void set_ByIllegalIndex_shouldThrowIndexOutOfBoundsException() {
+        dynamicArray.set(2, THIRD_TEST_STRING);
+    }
+
+    @Test
+    public void indexOfElement_shouldReturnIndexOrMinus1() {
+        assertEquals(0, dynamicArray.indexOf(FIRST_TEST_STRING));
+        assertEquals(1, dynamicArray.indexOf(SECOND_TEST_STRING));
+        assertEquals(-1, dynamicArray.indexOf(THIRD_TEST_STRING));
+    }
+
+    @Test
+    public void removeByIndex_shouldRemoveElementAndDecreaseSizeAndRearrangeElements() {
+        dynamicArray.remove(0);
+
+        assertFalse(dynamicArray.contains(FIRST_TEST_STRING));
+        assertEquals(1, dynamicArray.size());
+        assertEquals(SECOND_TEST_STRING, dynamicArray.get(0));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void removeByIllegalIndex_shouldThrowIndexOutOfBoundsException() {
+        dynamicArray.remove(2);
+    }
+
+    @Test
+    public void removeByElement_shouldRemoveElementAndDecreaseSizeAndRearrangeElements_andReturnTrue() {
+        boolean removed = dynamicArray.remove(FIRST_TEST_STRING);
+
+        assertTrue(removed);
+        assertFalse(dynamicArray.contains(FIRST_TEST_STRING));
+        assertEquals(1, dynamicArray.size());
+        assertEquals(SECOND_TEST_STRING, dynamicArray.get(0));
+    }
+
+    @Test
+    public void removeIllegalElement_shouldReturnFalse() {
+        assertFalse(dynamicArray.remove(THIRD_TEST_STRING));
+    }
+
+    @Test
+    public void clear_shouldRemoveElementsAndSetZeroSize() {
+        dynamicArray.clear();
+
+        assertFalse(dynamicArray.contains(FIRST_TEST_STRING));
+        assertFalse(dynamicArray.contains(SECOND_TEST_STRING));
+        assertTrue(dynamicArray.isEmpty());
+        assertEquals(0, dynamicArray.size());
+    }
+}
