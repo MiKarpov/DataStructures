9408701efe211b5a6f5cb3a3bacc67d53172a1c2
Mikhail Karpov <mikhailkarpov@hotmail.com>
2020-03-30 16:31:12 +0200

    Implemented Employee class

---
+++ src/main/java/com/mikhailkarpov/dto/Employee.java
@@ -0,0 +1,46 @@
+package com.mikhailkarpov.dto;
+
+public class Employee {
+
+    private final int id;
+    private final String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        return name.equals(employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/mikhailkarpov/list/SinglyLinkedListTest.java
@@ -0,0 +1,33 @@
+package com.mikhailkarpov.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SinglyLinkedListTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addToFront() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void removeFromFront() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void iterator() {
+    }
+}
